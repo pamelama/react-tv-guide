010df8aef77e3af0d2a1844ee6ecbacd1aadb37d
Pamela Anizio <aniziopamela@gmail.com>
2022-09-09 17:39:57 -0300

    Criacao dos componentes do Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer className="mt-10 bg-gray-800 py-5 text-center text-sm italic text-gray-500">
+      <p>
+        Created By
+        <a href="https://github.com/pamelama" className="underline">
+          Pamela Anizio
+        </a>
+        with Vite.js + React.js + React Router + React Icons + TailwindCSS
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
